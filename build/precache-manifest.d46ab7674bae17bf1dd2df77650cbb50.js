31db3596d0b32d3bb6dfdba60c2fa8fd773e847d
MarharytaKrasikava <marharytakrasikava@gmail.com>
2019-08-15 22:16:57 +0300

    make it better

---
+++ build/precache-manifest.d46ab7674bae17bf1dd2df77650cbb50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca168e2258b9bfa23021f98b3d14e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df46fbe2cae29c3b124",
+    "url": "/static/css/main.d929eacd.chunk.css"
+  },
+  {
+    "revision": "32e9157d675244dff7da",
+    "url": "/static/js/2.ff683205.chunk.js"
+  },
+  {
+    "revision": "4df46fbe2cae29c3b124",
+    "url": "/static/js/main.58aba832.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
