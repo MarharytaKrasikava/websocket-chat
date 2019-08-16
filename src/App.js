80aa4f32ec853e5d9bf8ac2e46c0c91717e65f3b
Marharyta Krasikava <marharytakrasikava@gmail.com>
2019-08-16 17:02:04 +0300

    feat: log when page is hidden

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import ReconnectingWebSocket from 'reconnectingwebsocket';
 import configureStore from './store/configureStore';
 import { addMessage } from './store/actions/messages';
 import { removeMessage } from './store/actions/messages';
 import { sortByDate } from './store/actions/filters'
 import MessageInput from './components/MessageInput/MessageInput';
 import MessageList from './components/MessageList/MessageList';
-import ReconnectingWebSocket from 'reconnectingwebsocket';
+import notify from './components/Notifications/Notifications';
 import './App.css';
 
 const store = configureStore();
 const socket = new ReconnectingWebSocket('wss://wssproxy.herokuapp.com/', null, { debug: false, reconnectInterval: 3000 });
 
-/* store.subscribe(() => {
-  console.log(store.getState());
-}) */
-
 socket.onmessage = function (event) {
   const messages = JSON.parse(event.data);
   let lastMessages;
   if (messages.length) {
-    if (messages[0].time > messages[messages.length - 1].time) {
+
+    if (messages[0].time > messages[messages.length - 1].time) { //выделяем последние 100 сообщений
       lastMessages = messages.slice(0, 99);
     } else { lastMessages = messages.slice(-100); }
     lastMessages.forEach((message) => {
@@ -29,12 +27,19 @@ socket.onmessage = function (event) {
 
     store.dispatch(sortByDate());
 
-    const storedMessages = store.getState().messages;
+    const storedMessages = store.getState().messages; //не даем store переполняться
     if (storedMessages.length >= 100) {
       store.dispatch(removeMessage({id: storedMessages[0].id}));
     }
+
   } else {
     store.dispatch(addMessage());
+  }
+  console.log(document.hidden)
+  if (document.hidden) {
+    const messages = store.getState().messages;
+    notify(messages[messages.length - 1]);
+    console.log(messages[messages.length - 1]);
   }
 }
 
